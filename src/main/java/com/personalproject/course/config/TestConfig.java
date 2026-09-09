4b8ce1c6b7140821ba38ed1008bf93cf4dbf8fbc
Kalil de A. Carvalho <kalilac@gmail.com>
2026-09-09 15:35:18 -0300

    JPA repository, dependency injection, database seeding - v2

--- src/main/java/com/personalproject/course/config/TestConfig.java
+++ src/main/java/com/personalproject/course/config/TestConfig.java
@@ -8,14 +8,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.personalproject.course.entities.User;
-import com.personalproject.course.repositories.Userrepository;
+import com.personalproject.course.repositories.UserRepository;
 
 @Configuration
 @Profile("test")
 public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
-	private Userrepository userRepository;
+	private UserRepository userRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
--- src/main/java/com/personalproject/course/repositories/UserRepository.java
+++ src/main/java/com/personalproject/course/repositories/UserRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.personalproject.course.entities.User;
 
-public interface Userrepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
