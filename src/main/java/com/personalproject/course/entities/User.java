bff3e3fd47331ef071bf9383018cae05f6ee0306
Kalil de A. Carvalho <kalilac@gmail.com>
2026-09-11 14:49:49 -0300

    Order, Instant, ISO 8601 - PART 1

--- src/main/java/com/personalproject/course/entities/User.java
+++ src/main/java/com/personalproject/course/entities/User.java
@@ -1,11 +1,14 @@
 package com.personalproject.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,6 +24,9 @@ public class User implements Serializable{
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {	
 	}
@@ -73,7 +79,11 @@ public class User implements Serializable{
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
+	
+	public List<Order> getOrders() {
+		return orders;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -98,5 +108,6 @@ public class User implements Serializable{
 			return false;
 		return true;
 	}
-	
+
+
 }
--- src/main/java/com/personalproject/course/resources/UserResource.java
+++ src/main/java/com/personalproject/course/resources/UserResource.java
@@ -22,7 +22,6 @@ public class UserResource {
 	@GetMapping
 	public ResponseEntity<List<User>> findAll() {
 		
-		//User u = new User(1L, "Maria", "maria@gmail.com", "999999", "12345");
 		List<User> list = service.findAll();
 		
 		return ResponseEntity.ok().body(list);
