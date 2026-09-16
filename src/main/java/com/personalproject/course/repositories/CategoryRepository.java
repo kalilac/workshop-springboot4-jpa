1b8f2abbc052a78e0291bbc86e3b2bae76cf5979
Kalil de A. Carvalho <kalilac@gmail.com>
2026-09-16 10:53:09 -0300

    Category

---
+++ src/main/java/com/personalproject/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.personalproject.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.personalproject.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
